18b56121d6c5ae18764ae4de9ca58ff48699cd5c
EspiraMarvin <espiraMarvin@gmail.com>
2023-04-09 00:34:05 +0300

    fix: removes unecessary repetitions

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMovieDto } from './create-movie.dto';
-import { IsString, IsNotEmpty } from 'class-validator';
 
-export class UpdateMovieDto extends PartialType(CreateMovieDto) {
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
